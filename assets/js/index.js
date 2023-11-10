ecd9bf773f1b88cc828c633916d22d164820dec0
Maca4536 <maca.esc.k@gmail.com>
2023-11-10 10:36:36 -0300

    Menú y Juego - Alertas + ajustes

---
+++ assets/js/index.js
@@ -0,0 +1,64 @@
+const btnLeft = document.getElementById("btn-left");
+const btnRight = document.getElementById("btn-right");
+const slider = document.getElementById("slider");
+const sliderSection = document.querySelectorAll(".slider-section");
+let contenedor_carrusel = document.getElementById("contenedor-carrusel");
+let como_jugar = document.getElementById("como_jugar");
+let aceptar = document.querySelectorAll(".aceptar");
+
+let operacion = 0;
+let widthImg = 100 / sliderSection.length;
+let posicion = 0;
+
+window.onload = () => {
+    document.querySelector("main").removeChild(contenedor_carrusel);
+}
+
+como_jugar.onclick = () => {
+    document.querySelector("main").appendChild(contenedor_carrusel);
+    if(posicion == sliderSection.length - sliderSection.length){
+        btnLeft.className = "deshabilitado";
+    }
+
+    document.getElementById("grid").className = "deshabilitado";
+}
+
+const moveToLeft = () => {
+    operacion -= widthImg;
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "all ease 0.5s";
+    btnRight.className = "";
+    posicion -=1;
+
+    if(posicion == sliderSection.length - sliderSection.length){
+        btnLeft.className = "deshabilitado";
+    }
+}
+
+const moveToRight = () => {
+    operacion += widthImg;
+    slider.style.transform = `translate(-${operacion}%)`;
+    slider.style.transition = "all ease 0.5s";
+    btnLeft.className = "";
+    posicion +=1;
+
+    if(posicion == sliderSection.length - 1){
+        btnRight.className = "deshabilitado";
+        console.log("Deshabilitado");
+    }
+}
+
+btnLeft.onclick = (e) => {
+    moveToLeft();
+}
+
+btnRight.onclick = (e) => {
+    moveToRight();
+}
+
+aceptar.forEach(boton => {
+    boton.onclick = () => {
+        document.querySelector("main").removeChild(contenedor_carrusel);
+        document.getElementById("grid").className = "";
+    }
+});
--- assets/js/juego.js
+++ assets/js/juego.js
@@ -8,20 +8,45 @@ const randomNumber = (min, max) => {
 /*** Función: Evaluar letra ***/
 
 const evaluarLetra = (letraFun, letraRandomFun) => {
-    if(letraFun == letraRandomFun){
-        console.log("Letra correcta.");
-        console.log("-----------------");
-        letraRandomFun = randomNumber(minimo, maximo);
-        console.log(`Letra generada: ${letraRandomFun}`);
-        console.log("-----------------");
-        return letraRandomFun;
-    }
+    if(posicion1 < 10){
+        if(letraFun == letraRandomFun){
+            console.log("Letra correcta.");
+            console.log("-----------------");
+    
+            posicion1 += 1;
+            console.log(posicion2);
+            pj1.style.gridColumn = posicion1 + "/" + (posicion1 + 1);
+            console.log(pj1.style.gridColumn = posicion1 + "/" + (posicion1 + 1));
+            console.log(posicion1);
+    
+            if(posicion1 < 10){
+                letraRandomFun = randomNumber(minimo, maximo);
+                console.log(`Letra generada: ${letraRandomFun}`);
+                console.log("-----------------");
+            }
 
-    else{
-        console.log("Letra incorrecta.");
-        console.log("-----------------");
-        console.log(letraRandomFun);
-        return letraRandomFun;
+            return letraRandomFun;
+        }
+    
+        else{
+            console.log("Letra incorrecta.");
+            console.log("-----------------");
+    
+            if(posicion1 != 1){
+                posicion1 -= 1;
+                console.log(posicion2);
+                pj1.style.gridColumn = posicion1 + "/" + (posicion1 + 1);
+            }
+            
+            console.log(pj1.style.gridColumn = posicion1 + "/" + (posicion1 + 1));
+            console.log(posicion1);
+    
+            letraRandomFun = randomNumber(minimo, maximo);
+            console.log(`Letra generada: ${letraRandomFun}`);
+            console.log("-----------------");
+
+            return letraRandomFun;
+        }
     }
     
 }
@@ -30,26 +55,96 @@ const evaluarLetra = (letraFun, letraRandomFun) => {
 /*** Función: Evaluar flecha ***/
 
 const evaluarFlecha = (flechaFun, flechaRandomFun) => {
-    if(flechaFun == flechaRandomFun){
-        console.log("Flecha correcta.");
-        console.log("-----------------");
-        flechaRandomFun = randomNumber(minimo, maximo);
-        console.log(`Flecha generada: ${flechaRandomFun}`);
-        console.log("-----------------");
-        return flechaRandomFun;
-    }
-
-    else{
-        console.log("Flecha incorrecta.");
-        console.log("-----------------");
-        console.log(flechaRandomFun);
-        return flechaRandomFun;
+    if(posicion2 < 10){
+        if(flechaFun == flechaRandomFun){
+            console.log("Flecha correcta.");
+            console.log("-----------------");
+    
+            posicion2 += 1;
+            console.log(posicion2);
+            pj2.style.gridColumn = posicion2 + "/" + (posicion2 + 1);
+            console.log(pj2.style.gridColumn = posicion2 + "/" + (posicion2 + 1));
+            console.log(posicion2);
+    
+            if(posicion2 < 10){
+                flechaRandomFun = randomNumber(minimo, maximo);
+                console.log(`Flecha generada: ${flechaRandomFun}`);
+                console.log("-----------------");
+            }
+            
+            return flechaRandomFun;
+        }
+    
+        else{
+            console.log("Flecha incorrecta.");
+            console.log("-----------------");
+            
+            if(posicion2 != 1){
+                posicion2 -= 1;
+                pj2.style.gridColumn = posicion2 + "/" + posicion2 + 1;
+            }
+    
+            console.log(pj2.style.gridColumn = posicion2 + "/" + (posicion2 + 1));
+            console.log(posicion2);
+    
+            flechaRandomFun = randomNumber(minimo, maximo);
+            console.log(`Flecha generada: ${flechaRandomFun}`);
+            console.log("-----------------");
+            
+            return flechaRandomFun;
+        }
     }
     
 }
 
+/*** Función: Evaluar ganador ***/
 
-/*** Función: Evaluar letra ***/
+const evaluarGanador = () => {
+    if(posicion1 == 10 || posicion2 == 10){
+        if(posicion1 == 10){
+             setTimeout(()=>{
+                Swal.fire({
+                    allowOutsideClick: false,
+                    allowEscapeKey: false,
+                    /* iconHtml: "https://cdn-icons-png.flaticon.com/512/1332/1332056.png", */
+                    title: "Ganador: Jugador 1\n¡Felicidades!",
+                    text: "¿Qué deseas hacer?",
+                    showDenyButton: true,
+                    confirmButtonText: "Volver a jugar",
+                    denyButtonText: `Ir al menú principal`
+                  }).then((result) => {
+                    /* Read more about isConfirmed, isDenied below */
+                    if (result.isConfirmed) {
+                      window.location.reload();
+                    } else if (result.isDenied) {
+                      location.href = "../index.html"
+                    }
+                  });
+            }, 500);
+        }
+    
+        else{
+            setTimeout(()=>{
+                Swal.fire({
+                    allowOutsideClick: false,
+                    allowEscapeKey: false,
+                    title: "Ganador: Jugador 2\n¡Felicidades!",
+                    text: "¿Qué deseas hacer?",
+                    showDenyButton: true,
+                    confirmButtonText: "Volver a jugar",
+                    denyButtonText: `Ir al menú principal`
+                  }).then((result) => {
+                    /* Read more about isConfirmed, isDenied below */
+                    if (result.isConfirmed) {
+                      window.location.reload();
+                    } else if (result.isDenied) {
+                      location.href = "../index.html"
+                    }
+                  });
+            }, 500);
+        }
+    }
+}
 
 
 /*** Declaración de variables ***/
@@ -58,17 +153,21 @@ const minimo = 0;
 const maximo = 4;
 let letraRandom = randomNumber(minimo, maximo);
 let flechaRandom = randomNumber(minimo, maximo);
+let letra;
+let flecha;
+let teclaPresionada;
+let pj1 = document.getElementById("PJ1");
+let pj2 = document.getElementById("PJ2");
+let posicion1 = 1;
+let posicion2 = 1;
+
+
+/*** Juego ***/
+
 console.log(`Letra generada: ${letraRandom}`);
 console.log("-----------------");
 console.log(`Flecha generada: ${flechaRandom}`);
 console.log("-----------------");
-let letra;
-let flecha;
-let teclaPresionada;
-let ganador = 0;
-
-
-/*** Juego ***/
 
 document.onkeyup = (event) => {
     teclaPresionada = event.key.toLowerCase();
@@ -138,4 +237,7 @@ document.onkeyup = (event) => {
             flechaRandom = evaluarFlecha(flecha, flechaRandom);
             break;
     }
+
+    evaluarGanador();
+
 }
